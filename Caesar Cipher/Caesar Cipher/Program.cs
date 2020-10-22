0b9b47c391280c6acd4061d72b555a193717a392
kingjosh0615 <54904560+kingjosh0615@users.noreply.github.com>
2020-10-22 18:20:07 -0400

    Done :)

--- Caesar Cipher/Caesar Cipher/Program.cs
+++ Caesar Cipher/Caesar Cipher/Program.cs
@@ -10,9 +10,34 @@ namespace Caesar_Cipher
     {
         static void Main(string[] args)
         {
+            bool mainRunning = true;
+            while (mainRunning)
+            {
+                Console.WriteLine("Welcome to the program. Would you like to encrypt a message or decrypt a message? Type the letter 'E' to choose encrypt and 'D' to choose decrypt.");
+                string userInput = (Console.ReadLine()).ToLower();
+                if (userInput == "e")
+                {
+                    mainRunning = false;
+                    Encrypter();
+                }
+                else if (userInput == "d")
+                {
+                    mainRunning = false;
+                    Decrypter();
+                }
+                else
+                {
+                    mainRunning = true;
+                    Console.Clear();
+                    Console.WriteLine("Invalid input, try again.");
+                }
+            }
+        }
+        public static void Encrypter()
+        {
             char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-            Console.WriteLine("Input your secret message.");
+            Console.WriteLine("Input your message to be encoded.");
             string userMessage = Console.ReadLine().ToLower();
             char[] secretMessage = userMessage.ToCharArray();
             char[] encryptedMessage = new char[secretMessage.Length];
@@ -26,6 +51,27 @@ namespace Caesar_Cipher
             }
             string userMessageEncrypted = String.Join("", encryptedMessage);
             Console.WriteLine($"This is your message encrypted, \"{userMessageEncrypted}\". ");
+            Console.ReadLine();
+        }
+        public static void Decrypter()
+        {
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            Console.WriteLine("Input your message to be decoded.");
+            string userMessage = Console.ReadLine().ToLower();
+            char[] secretMessage = userMessage.ToCharArray();
+            char[] encryptedMessage = new char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                char tempLetter = secretMessage[i];
+                int tempIndexOld = Array.IndexOf(alphabet, tempLetter);
+                int tempIndexNew = (tempIndexOld - 3) % alphabet.Length;
+                encryptedMessage[i] = alphabet[tempIndexNew];
+            }
+            string userMessageEncrypted = String.Join("", encryptedMessage);
+            Console.WriteLine($"This is your message decrypted, \"{userMessageEncrypted}\". ");
+            Console.ReadLine();
         }
     }
 }
